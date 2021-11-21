3b334677f28be0f14dcdfc30745248ccad401b82
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-21 18:15:05 +0530

    [Rutwik]Added:MaximumInt File

---
+++ GenericMaximum/MaximumComputation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsMaximum
+{
+    public class MaximumComputation
+    {
+        public static int FindMax(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            else
+            {
+                return thirdNumber;
+            }
+        }
+    }
+}
---
+++ GenericMaximum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GenericsMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome To Generic Maximum Problems");
+                Console.WriteLine("Enter the Program that want to be executed : \n 1. Maximum Integer \n 2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First number, Second number, Third number : ");
+                        int first = Convert.ToInt32(Console.ReadLine());
+                        int second = Convert.ToInt32(Console.ReadLine());
+                        int third = Convert.ToInt32(Console.ReadLine());
+                        int result = MaximumComputation.FindMax(first, second, third);
+                        Console.WriteLine("The Maximum Number is : " + result);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
